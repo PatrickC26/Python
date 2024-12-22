ff5f0a579821b0ac03f845befa3a8cf4ca835174
PatrickC26 <patrick910826@gmail.com>
2024-12-22 16:20:23 +0800

    optimize setting page

--- LINE_official/page.py
+++ LINE_official/page.py
@@ -16,26 +16,6 @@ I_renew = None
 # Sample data array
 F_userList, L_lineAccountInfo, lineAccount = None, None, None
 
-def updateLineAccount():  # Tokens and Urls
-    global lineAccount
-    try:
-        error_Handling.error_info_Handling(100, "[INFO] Verifying LINE account")
-        channel_access_token_public = 'ufRWLckiARfsXwB0JN2DjEcFVUtFzPpphhR6YhCAIYou9T8Y3mZ0qTOEQVCB/QPuLiS08ybRL2XEj20AW4dX2loBM3jZkNE4nWom+KyhDlSn3tQZD4afDPns0OmYHME3tNrWf9Gk48aNaWQYnYuDMwdB04t89/1O/w1cDnyilFU='
-        webhook_id_ = "ee481c17c8ae36266612fe4d74bbd685"
-        webhook_bearer_ = "22381d5762fbc834e57504cd2e541f821bd7a46ebd432d2d"
-
-        lineAccount = official_LINE_lib.LineOfficialAccount(LINE_channel_access_token=channel_access_token_public,
-                                                            webhook_id=webhook_id_,
-                                                            webhook_bearer=webhook_bearer_, key="Luósī³")
-        error_Handling.error_info_Handling(200, "[INFO] Verify LINE account succeed")
-    except Exception as e:
-        error_Handling.error_info_Handling(-1, "[ERROR] Failed to verify LINE account with error", e)
-
-def renew():
-    global isRenewing
-    isRenewing = True
-
-
 def updateInfo():
     global isRenewing, F_userList, L_lineAccountInfo, lineAccount
 
@@ -51,34 +31,24 @@ def updateInfo():
         image_label.grid(row=idx, column=0, padx=0, pady=0)
 
         # Right column: Name
-        name_label = tk.Label(F_userList, text=user[2], bg=None, font=("Arial", 14))
+        name_label = tk.Label(F_userList, text=user[2], font=("Arial", 14))
         name_label.grid(row=idx, column=1, padx=20, pady=50)
     isRenewing = False
 
+def updateLineAccount():  # Tokens and Urls
+    global lineAccount
+    try:
+        error_Handling.error_info_Handling(100, "[INFO] Verifying LINE account")
+        channel_access_token_public = 'ufRWLckiARfsXwB0JN2DjEcFVUtFzPpphhR6YhCAIYou9T8Y3mZ0qTOEQVCB/QPuLiS08ybRL2XEj20AW4dX2loBM3jZkNE4nWom+KyhDlSn3tQZD4afDPns0OmYHME3tNrWf9Gk48aNaWQYnYuDMwdB04t89/1O/w1cDnyilFU='
+        webhook_id_ = "ee481c17c8ae36266612fe4d74bbd685"
+        webhook_bearer_ = "22381d5762fbc834e57504cd2e541f821bd7a46ebd432d2d"
 
-def process_updateInfo():
-    global isRenewing, isShuttingDown
-    error_Handling.error_info_Handling(200, "[INFO] hosting updateInfo")
-    while 1:
-        try:
-            if isShuttingDown:
-                error_Handling.error_info_Handling(0, "[INFO] Shutting down process updater")
-                break
-            if isRenewing:
-                updateInfo()
-            time.sleep(.2)
-        except Exception as e:
-            error_Handling.error_info_Handling(-1, "[ERROR] updateInfo failed with error", e)
-            time.sleep(1)
-
-
-def open_settings():
-    global isRenewing
-    error_Handling.error_info_Handling(200, "[INFO] Settings button clicked")
-    # updateLineAccount()
-    Root_settingsPage.deiconify()
-
-
+        lineAccount = official_LINE_lib.LineOfficialAccount(LINE_channel_access_token=channel_access_token_public,
+                                                            webhook_id=webhook_id_,
+                                                            webhook_bearer=webhook_bearer_, key="Luósī³")
+        error_Handling.error_info_Handling(200, "[INFO] Verify LINE account succeed")
+    except Exception as e:
+        error_Handling.error_info_Handling(-1, "[ERROR] Failed to verify LINE account with error", e)
 
 Root_mainPage = tk.Tk()
 def init_main_page():
@@ -113,15 +83,25 @@ def init_main_page():
     F_top.pack(side=tk.RIGHT)
 
     # Renew Button
+    def btnCmd_refresh():
+        global isRenewing
+        isRenewing = True
+        
     I_renew = Image.open(folder + "renew.png").resize((50, 50))
     P_renew = ImageTk.PhotoImage(I_renew)
-    B_renew = tk.Button(F_top, text="renew", command=renew, image=P_renew)
+    B_renew = tk.Button(F_top, text="renew", command=btnCmd_refresh, image=P_renew)
     B_renew.image = P_renew  # Keep a reference to avoid garbage collection
     B_renew.pack(side=tk.LEFT, padx=5)
 
     # Settings Button
+    def btnCmd_open_settings():
+        global isRenewing, Root_settingsPage
+        error_Handling.error_info_Handling(200, "[INFO] Settings button clicked")
+        # updateLineAccount()
+        Root_settingsPage.deiconify()
+        
     P_setting = ImageTk.PhotoImage(Image.open(folder + "setting.png").resize((50, 50)))
-    B_setting = tk.Button(F_top, text="setting", command=open_settings, image=P_setting)
+    B_setting = tk.Button(F_top, text="setting", command=btnCmd_open_settings, image=P_setting)
     B_setting.image = P_setting  # Keep a reference to avoid garbage collection
     B_setting.pack(side=tk.LEFT, padx=5)
     # ======= top Frame =======
@@ -157,8 +137,6 @@ def init_main_page():
 
     error_Handling.error_info_Handling(200, "[INFO] page init DONE")
 
-
-# Root_mainPage.withdraw()
 Root_settingsPage = tk.Toplevel(Root_mainPage)
 def init_settings_page():
     global Root_settingsPage, Root_mainPage
@@ -203,7 +181,12 @@ def init_settings_page():
     bottom_frame = tk.Frame(Root_settingsPage)
     bottom_frame.pack(side=tk.BOTTOM, fill=tk.Y, expand=True, padx=10, pady=2)
 
-    B_confirm = tk.Button(bottom_frame, text="確認", command=save_settings)
+    def btnCmd_save_settings():
+        error_Handling.error_info_Handling(200, "[INFO] settings saved")
+        Root_settingsPage.withdraw()
+        updateLineAccount()
+
+    B_confirm = tk.Button(bottom_frame, text="確認", command=btnCmd_save_settings)
     B_confirm.pack(side=tk.RIGHT, padx=5)
 
     # ======= bottom frame =======
@@ -215,12 +198,6 @@ def init_settings_page():
 
     # Root_settingsPage. TODO make page uneditable
     error_Handling.error_info_Handling(200, "[INFO] settings page init DONE")
-
-
-def save_settings():
-    error_Handling.error_info_Handling(200, "[INFO] settings saved")
-    Root_settingsPage.withdraw()
-    updateLineAccount()
 
 def process_loading_animation():
     global isRenewing, B_renew, I_renew, isShuttingDown
@@ -254,6 +231,21 @@ def process_loading_animation():
             error_Handling.error_info_Handling(-1, "[ERROR] loading animation failed with error", e)
             time.sleep(.5)
             pass
+
+def process_updateInfo():
+    global isRenewing, isShuttingDown
+    error_Handling.error_info_Handling(200, "[INFO] hosting updateInfo")
+    while 1:
+        try:
+            if isShuttingDown:
+                error_Handling.error_info_Handling(0, "[INFO] Shutting down process updater")
+                break
+            if isRenewing:
+                updateInfo()
+            time.sleep(.2)
+        except Exception as e:
+            error_Handling.error_info_Handling(-1, "[ERROR] updateInfo failed with error", e)
+            time.sleep(1)
 
 
 init_main_page()
